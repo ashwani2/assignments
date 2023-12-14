ce0d58a9f9762e91bb7b22b0c5b46aa67c483f80
ashwani2 <ashwani.kumar@1406@gmail.com>
2023-12-14 12:13:25 +0530

    express solution

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,48 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+app.use(express.json());
+
+app.get("/files", async (req, res) => {
+  try {
+    let directoryPath = path.join(__dirname, "files");
+    let fileNameArray = await new Promise((resolve, reject) => {
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) reject(err);
+        else resolve(files);
+      });
+    });
+    res.status(200).send(fileNameArray);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.get("/file/:filename", async (req, res) => {
+  try {
+    let filePath = path.join(__dirname, "files", req.params.filename);
+    let str = await new Promise((resolve, reject) => {
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) reject(err);
+        else {
+          resolve(data);
+        }
+      });
+    });
+
+    res.status(200).send(str);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000);
 
 module.exports = app;
